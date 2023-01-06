696645988468435c6c82f3b33c4e90d34054c58a
amitay <amitay1599@gmail.com>
2023-01-06 23:46:16 +0200

    naming

--- nyt_api/index.js
+++ nyt_api/index.js
@@ -1,14 +1,13 @@
-require('dotenv').config()
+require("dotenv").config();
 const axios = require("axios");
 const { NYT_API_BASE_URL } = require("./consts");
 
-const getSoriesData = async (route, index) => {
+const getStories = async (route, index) => {
   let url = `${NYT_API_BASE_URL}${route}.json?api-key=${process.env.NYC_API_KEY}`;
-  const data = await axios.get(url).then((response) => response.data)
+  const data = await axios.get(url).then((response) => response.data);
   const title = data.results[index].title;
   const abstractData = data.results[index].abstract;
-  return {title, abstractData};
+  return { title, abstractData };
 };
 
-
-module.exports = getSoriesData;
+module.exports = getStories;
--- routes/storiesRoutes.js
+++ routes/storiesRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { NYT_API_ROUTES } = require("../nyt_api/consts");
-const getSoriesData = require("../nyt_api");
+const getStories = require("../nyt_api");
 const askOpenAi = require("../openAi_api");
 
 router.get("/us/:index", async (req, res) => {
   const { index } = req.params;
-  const homeData = await getSoriesData(NYT_API_ROUTES.US, index);
+  const homeData = await getStories(NYT_API_ROUTES.US, index);
   const { title, abstractData } = homeData;
   const openRes = await askOpenAi(title, abstractData);
   res.status(200).send(openRes);
@@ -14,7 +14,7 @@ router.get("/us/:index", async (req, res) => {
 
 router.get("/science/:index", async (req, res) => {
   const { index } = req.params;
-  const homeData = await getSoriesData(NYT_API_ROUTES.SCIENCE, index);
+  const homeData = await getStories(NYT_API_ROUTES.SCIENCE, index);
   const { title, abstractData } = homeData;
   const openRes = await askOpenAi(title, abstractData);
   res.status(200).send(openRes);
@@ -22,7 +22,7 @@ router.get("/science/:index", async (req, res) => {
 
 router.get("/arts/:index", async (req, res) => {
   const { index } = req.params;
-  const homeData = await getSoriesData(NYT_API_ROUTES.ARTS, index);
+  const homeData = await getStories(NYT_API_ROUTES.ARTS, index);
   const { title, abstractData } = homeData;
   const openRes = await askOpenAi(title, abstractData);
   res.status(200).send(openRes);
